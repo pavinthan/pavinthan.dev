c1a812dd72a4faca772b17ec9a5f7e46a13c585f
Pavinthan <pavinthan@outlook.com>
2019-09-29 18:59:56 +0530

    page title added

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,9 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Pavinthan`,
+    siteUrl: `https://pavinthan.dev`,
+    description: `A blog by Pavinthan`,
+  },
 }
