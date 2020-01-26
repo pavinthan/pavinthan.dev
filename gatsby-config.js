313a0604e7989c48b9ec8de183dd4a008b27e553
Pavinthan <pavinthan@outlook.com>
2020-01-27 01:59:24 +0530

    build issues fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require(`path`)
+
 module.exports = {
   siteMetadata: {
     title: `Pavinthan Sivakumar`,
@@ -74,7 +76,10 @@ module.exports = {
         tailwind: true, // Enable tailwindcss support
         // whitelist: ['whitelist'], // Don't remove this selector
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders,
+        content: [
+          path.join(process.cwd(), "src/**/!(*.d).{ts,js,jsx,tsx,md,mdx}"),
+        ],
       },
     },
     "gatsby-plugin-dark-mode",
