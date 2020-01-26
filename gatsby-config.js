7910d574e35d6f9705e354344b7e48a0722e79c4
Pavinthan <pavinthan@outlook.com>
2020-01-27 02:04:13 +0530

    build issues fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,15 +71,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-purgecss`,
       options: {
-        printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders,
-        content: [
-          path.join(process.cwd(), "src/**/!(*.d).{ts,js,jsx,tsx,md,mdx}"),
-        ],
+        printRejected: false,
+        develop: false,
+        tailwind: true,
       },
     },
     "gatsby-plugin-dark-mode",
